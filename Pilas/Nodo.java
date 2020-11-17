3e209083a4e7410020bc9bcfe4fe26a4d8913ba3
Juan Pablo Campos <64780248+Jp3222@users.noreply.github.com>
2020-11-17 14:54:53 -0600

    Create Nodo.java

---
+++ Pilas/Nodo.java
@@ -0,0 +1,13 @@
+package unidad3_pilas_tarea;
+
+public class Nodo {
+
+    int Dato;
+    Nodo siguiente;
+
+    public Nodo(int D) {
+        this.Dato = D;
+        this.siguiente = null;
+    }
+    
+}
